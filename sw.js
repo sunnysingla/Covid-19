ae4d9142e6f60659cd0f4cca2f5b1e16a24569ff
sunnysingla <ssingla1985@gmail.com>
2020-05-04 00:19:09 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -3,9 +3,7 @@ self.addEventListener('install', function (e) {
    caches.open('video-store').then(function(cache) {
      return cache.addAll([
        '/Covid-19/',
-         '/Covid-19/index.html',
-         '/Covid-19/index.js',
-         '/Covid-19/sw.js'
+         '/Covid-19/index.html' 
      ]);
    })
  );
